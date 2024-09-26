52d9c3abcd1199db095c8099206b83ec40a66d80
Dmambo <mambotrader0@gmail.com>
2024-09-26 18:21:37 +0530

    Removed the border for the charts

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -6,7 +6,7 @@ const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("rounded-xl    shadow", className)} {...props} />
+  <div ref={ref} className={cn( className)} {...props} />
 ));
 Card.displayName = "Card";
 
